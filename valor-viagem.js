304d8a04ff61a2fd1c38699eb7ffb2d6984eea5c
André <andre.vicente@teknisa.com>
2022-10-23 21:58:48 -0300

    primeiro commit

---
+++ valor-viagem.js
@@ -0,0 +1,21 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+1- Preço do combustível;
+2- Gasto médio de combustível do carro;
+3- Distancia em KM da viagem;
+
+Imprima no console o valor que será gasto de combustível  para realizar esta viagem.
+
+*/
+const precoGasolina = 4.79;
+const kmPorLitros = 42;
+const distanciaEmKm = 440;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+const valorDaViagem = litrosConsumidos * precoGasolina;
+
+console.log('R$ ' + valorDaViagem.toFixed(2));
+
+
